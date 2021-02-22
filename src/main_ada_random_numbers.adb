ae94707f219745cc992773dc5d312049e60dd505
wruslan <wruslangit@gmail.com>
2021-02-22 09:30:06 +0800

    retest random numbers

--- src/main_ada_random_numbers.adb
+++ src/main_ada_random_numbers.adb
@@ -34,24 +34,28 @@ begin
    PADTS.dtstamp; ATIO.Put_Line ("Bismillah 3 times WRY");
    PADTS.dtstamp; ATIO.Put_Line ("Running inside GNAT Studio Community");
    ATIO.New_Line;  
-     
+   
+   ATIO.Put_Line("PAR.generate_random_integer;");
    for idx in 1..5 loop
       PAR.generate_random_integer;
    end loop;
      
-   ATIO.New_Line; 
+   ATIO.New_Line;
+   ATIO.Put_Line("PAR.generate_random_float;");
    for idx in 1..5 loop
       PAR.generate_random_float;
    end loop;
    
    ATIO.New_Line; 
+   ATIO.Put_Line("random_int := PAR.get_random_integer (-20, +12);");
    for idx in 1..5 loop
       -- random_int := PAR.get_random_integer (10, 20);
-      random_int := PAR.get_random_integer (-20, +2);
+      random_int := PAR.get_random_integer (-20, +12);
       ATIO.Put_Line("random_int = " & Integer'Image(random_int));
    end loop;
    
-   ATIO.New_Line;   
+   ATIO.New_Line; 
+   ATIO.Put_Line("random_float := PAR.get_random_float (-3.5, +3.5);");
    for idx in 1..5 loop
       -- random_float := PAR.get_random_float (2.5, 3.5);
       random_float := PAR.get_random_float (-3.5, +3.5);
@@ -63,3 +67,40 @@ begin
 -- ========================================================   
 end main_ada_random_numbers;
 -- ========================================================
+
+-- EXECUTION
+-- /home/wruslan/github-ump/ada-random-numbers/exec/main_ada_random_numbers.adx
+-- 2021-02-22 01:17:56.98054949118 Bismillah 3 times WRY
+-- 2021-02-22 01:17:56.98055140300 Running inside GNAT Studio Community
+
+-- PAR.generate_random_integer;
+-- Random integer (1..100) =  17
+-- Random integer (1..100) =  84
+-- Random integer (1..100) =  65
+-- Random integer (1..100) =  27
+-- Random integer (1..100) =  57
+
+-- PAR.generate_random_float;
+-- Random float (0.0 .. 1.0) =  6.40517E-01
+-- Random float (0.0 .. 1.0) =  5.51473E-02
+-- Random float (0.0 .. 1.0) =  2.45588E-01
+-- Random float (0.0 .. 1.0) =  2.37335E-01
+-- Random float (0.0 .. 1.0) =  1.08305E-01
+
+-- random_int := PAR.get_random_integer (-20, +12);
+-- random_int =  12
+-- random_int = -5
+-- random_int =  10
+-- random_int = -7
+-- random_int = -17
+
+-- random_float := PAR.get_random_float (-3.5, +3.5);
+-- random_float =  1.90540E+00
+-- random_float =  2.39291E+00
+-- random_float = -3.31671E+00
+-- random_float =  2.48616E+00
+-- random_float = -3.26613E+00
+
+-- 2021-02-22 01:17:56.98055367788 Alhamdulillah 3 times WRY
+-- [2021-02-22 09:17:57] process terminated successfully, elapsed time: 00.20s
+
